219e063a6e263c0d1ff6c44ebb115241f2c848f4
tosumaso <wakuwaku5712@gmail.com>
2021-09-26 14:44:11 +0900

    first commit

---
+++ src/main/java/MapTraveler/develop/MapTravelerApplication.java
@@ -0,0 +1,13 @@
+package MapTraveler.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapTravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapTravelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MapTraveler/develop/MapTravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package MapTraveler.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapTravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
